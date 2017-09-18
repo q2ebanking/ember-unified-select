6fc9385dba80ddfc20b9c6b93867c208bd9f0746
Abdul Hamid <ahamid@ahamid-mbp.austin.q2technology.com>
2017-09-18 14:12:50 -0500

    Reducing template logic and moving dependencies

---
+++ blueprints/ember-unified-select/index.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = {
+    name: '../ember-unified-select',
+    /*afterInstall() {
+        console.log('------------------------------------afterInstall-------------------------------------------');
+        return this.addPackagesToProject([
+            { name: 'ember-truth-helpers' },
+            { name: 'ember-responsive' },
+            { name: 'ember-cli-sass' }
+        ]);
+    }*/
+};
